c8a6f829251b4de6943c5197ac26e0733a56566d
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-05-02 18:16:42 -0700

    Classes hurt

---
+++ student.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+#include <vector>
+
+using namespace std;
+
+void student::setName(string first, string last)
+{
+  firstName = first;
+  lastName = last;
+}
+
+string student::fullName()
+{
+  firstName.append(" ");
+  firstName.append(lastName);
+  return firstName;
+}
+
+void student::addGrade(double grade)
+{
+  grid.push_back(grade);
+}
+
+double student::getScore()
+{
+  score = 0;
+  temporary = 0;
+
+  for(int i=0; i<grid.size(); i++)
+  {
+    temporary = score + grid[i];
+    score = temporary;
+  }
+  score = score/grid.size();
+  return score;
+}
